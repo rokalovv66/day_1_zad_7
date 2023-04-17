881c40e2371e893e1656a96fc83f77a97ecdc5ef
Нисик <@DESKTOP-ALORV4C>
2023-04-17 14:23:53 +0300

    Добавьте файлы проекта.

---
+++ day 1 (7)/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = 2; // задаем значение переменной a
+        int b = 5; // задаем значение переменной b
+        int result = 1; // задаем начальное значение для произведения
+
+        for (int i = a; i <= b; i++)
+        {
+            result *= i; // умножаем текущее значение произведения на i
+        }
+
+        Console.WriteLine("Произведение всех целых чисел от {0} до {1} равно {2}", a, b, result);
+        Console.ReadLine();
+    }
+}
